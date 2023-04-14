73fdbe6b0efb2f71754f039522e761e4450e79ac
Thirumalesh <thirumaleshalkachenu.com>
2023-04-14 13:04:53 +0530

    initial commit

---
+++ src/main/java/in/ashokit/dao/ReportDao.java
@@ -0,0 +1,13 @@
+package in.ashokit.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ReportDao {
+	
+	public ReportDao()
+	{
+		System.out.println("reportdao con");
+	}
+
+}
